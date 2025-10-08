88e2387896cc1b791ac5af46551bc0a06c8ccc6d
phuchungle2909 <hunglpph48998@gmail.com>
2025-10-09 01:14:49 +0700

    them slider banner vao trang chu

--- src/pages/home/HomePage.tsx
+++ src/pages/home/HomePage.tsx
@@ -1,5 +1,14 @@
+import Banner from "./components/Banner";
+
 const HomePage = () => {
-  return <div>Home Page</div>;
+  return (
+    <>
+      <section>
+        <Banner />
+      </section>
+      <section id="booking"></section>
+    </>
+  );
 };
 
 export default HomePage;
