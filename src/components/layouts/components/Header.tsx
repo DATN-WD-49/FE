5e0b5e2e29965ca5bf9110b52ebd733af36e8558
phuchungle2909 <hunglpph48998@gmail.com>
2025-10-09 08:51:43 +0700

    them tinh nang scroll den booking section

--- src/components/layouts/components/Header.tsx
+++ src/components/layouts/components/Header.tsx
@@ -1,6 +1,9 @@
 import { Link } from "react-router";
+import { scrollToSection } from "../../../common/utils";
 
 const Header = () => {
+  const handleBooking = () => scrollToSection("booking");
+
   return (
     <header className="bg-white">
       <div className="max-w-[1280px] mx-6  xl:mx-auto flex justify-between items-center">
@@ -12,14 +15,14 @@ const Header = () => {
           />
         </div>
         <div className="flex items-center gap-3">
-          <a
-            href="#booking"
-            className="bg-[#0c7d41] font-medium py-3 px-6 uppercase rounded-full text-white"
+          <button
+            onClick={handleBooking}
+            className="bg-[#0c7d41] font-medium hover:opacity-85 duration-300 cursor-pointer py-3 px-6 uppercase rounded-full text-white"
           >
             Đặt vé ngay
-          </a>
+          </button>
           <Link
-            className="bg-[#0c7d41] font-medium py-3 px-6 uppercase rounded-full text-white"
+            className="bg-[#0c7d41] font-medium hover:opacity-85 duration-300 py-3 px-6 uppercase rounded-full text-white"
             to={"/login"}
           >
             Đăng nhập
