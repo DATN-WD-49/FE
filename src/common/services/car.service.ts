f2a60a1d50a494835fe3b0bccf9c00c17e97ed8c
phuchungle2909 <hunglpph48998@gmail.com>
2025-10-28 17:57:10 +0700

    Chuc nang tao moi xe

--- src/common/services/car.service.ts
+++ src/common/services/car.service.ts
@@ -1,11 +1,19 @@
 import type { IParams } from "../types";
-import type { ICar } from "../types/Car";
+import type { ICar, ICarPayload } from "../types/Car";
 import type { IResponse } from "../types/Response";
 import api from "../utils/api";
+
 export const getAllcar = async (
   params?: IParams,
 ): Promise<IResponse<ICar[]>> => {
   const { data } = await api.get("/car", { params });
+  return data;
+};
+
+export const createCar = async (
+  payload: ICarPayload,
+): Promise<IResponse<ICar>> => {
+  const { data } = await api.post("/car", payload);
   return data;
 };
 
