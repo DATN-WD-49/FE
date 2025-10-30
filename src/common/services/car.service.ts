0c69c7fdc87d7007857a951f44a8774e8ef4a737
TuyenDG <dinhgiatuyen2212@gmail.com>
2025-10-30 08:44:39 +0700

    chuc nang updateCar

--- src/common/services/car.service.ts
+++ src/common/services/car.service.ts
@@ -3,6 +3,13 @@ import type { ICar, ICarPayload } from "../types/Car";
 import type { IResponse } from "../types/Response";
 import api from "../utils/api";
 
+export const updateCar = async (
+  carID: string,
+  payload: Omit<ICarPayload, "floor">,
+) => {
+  const { data } = await api.patch(`/car/update/${carID}`, payload);
+  return data;
+};
 export const getAllcar = async (
   params?: IParams,
 ): Promise<IResponse<ICar[]>> => {
@@ -18,6 +25,11 @@ export const createCar = async (
 };
 
 export const updateStatuscar = async (id: string): Promise<IResponse<ICar>> => {
+  const { data } = await api.patch(`/car/status/${id}`);
+  return data;
+};
+
+export const getDetailCar = async (id: string): Promise<IResponse<ICar>> => {
   const { data } = await api.patch(`/car/status/${id}`);
   return data;
 };
