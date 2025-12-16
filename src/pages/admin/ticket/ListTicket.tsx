1f10a77e14ec1f89ba54628c1b71e2b5e2309d26
phuchungle2909 <hunglpph48998@gmail.com>
2025-12-16 14:24:38 +0700

    fix_list-ticket-filter

--- src/pages/admin/ticket/ListTicket.tsx
+++ src/pages/admin/ticket/ListTicket.tsx
@@ -1,51 +1,58 @@
-import React from 'react'
-import { useTable } from '../../../common/hooks/useTable';
-import { getAllOrder } from '../../../common/services/order.service';
-import { Link } from 'react-router';
-import { QrcodeOutlined } from '@ant-design/icons';
-import { Table } from 'antd';
-import { columnTicket } from './components/ColumnTicket';
-import FilterTicket from './components/FilterTicket';
-import { useQuery } from '@tanstack/react-query';
-import { QUERY_KEY } from '../../../common/contants/queryKey';
+import { useTable } from "../../../common/hooks/useTable";
+import type { IOrder } from "../../../common/types/Order";
+import { useQuery } from "@tanstack/react-query";
+import { QUERY_KEY } from "../../../common/contants/queryKey";
+import { getAllOrder } from "../../../common/services/order.service";
+import { Link } from "react-router";
+import { QrcodeOutlined } from "@ant-design/icons";
+import TableCustom from "../../../components/common/TableCustom";
+import FilterTicket from "./components/FilterTicket";
+import { columnTicket } from "./components/ColumnTicket";
 
 const ListTicket = () => {
-    const { query, getSorterProps } = useTable();
-    const { data } = useQuery({
-        queryKey: [
-            QUERY_KEY.ORDER.ROOT,
-            ...Object.keys(query),
-            ...Object.values(query),
-          ],
-          queryFn: () =>
-          getAllOrder({
-            searchFields: [
-                "customerInfo.userName",
-                "customerInfo.phone",
-                "customerInfo.email",
-              ],
-              ...query,
-          })
-    });
+  const { query, getSorterProps, onFilter, onSelectPaginateChange } =
+    useTable<IOrder>();
+  const { data: response, isLoading } = useQuery({
+    queryKey: [
+      QUERY_KEY.ORDER.ROOT,
+      ...Object.keys(query),
+      ...Object.values(query),
+    ],
+    queryFn: () =>
+      getAllOrder({
+        searchFields: [
+          "customerInfo.userName",
+          "customerInfo.phone",
+          "customerInfo.email",
+        ],
+        ...query,
+      }),
+  });
+  const { data = [], meta } = response || {};
   return (
     <div className="bg-white w-full min-h-[70dvh] rounded-md shadow-sm px-6 py-4">
-        <div className="flex items-center justify-between mb-4">
+      <div className="flex items-center justify-between mb-4">
         <h3 className="text-xl font-semibold mb-4">Quản lý vé đã đặt</h3>
         <Link to={`/admin/ticket/scan`} className="text-4xl!">
           <QrcodeOutlined />
         </Link>
-        </div>
-        <FilterTicket />
-        <div className="mt-4">
-        <Table
-          bordered
+      </div>
+      <FilterTicket />
+      <div className="mt-4">
+        <TableCustom<IOrder>
+          showPagination={(meta?.total as number) > 10}
+          isLoading={isLoading}
           columns={columnTicket(getSorterProps)}
-          dataSource={data?.data}
+          dataSource={data || []}
+          onFilter={onFilter}
+          onSelectPaginateChange={onSelectPaginateChange}
+          pageSize={meta?.limit || 10}
+          totalDocs={meta?.total}
+          currentPage={meta?.page || 1}
         />
-        </div>
-
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default ListTicket
+export default ListTicket;
