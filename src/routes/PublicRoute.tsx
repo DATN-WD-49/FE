ed652edc2b1b98492f60415179bba18369d808a1
phuchungle2909 <hunglpph48998@gmail.com>
2025-12-19 10:55:07 +0700

    Thong tin ve chi tiet

--- src/routes/PublicRoute.tsx
+++ src/routes/PublicRoute.tsx
@@ -14,6 +14,7 @@ import Profile from "../pages/profile/Profile";
 import ProfilePage from "../pages/profile/ProfilePage";
 import ProfileIndex from "../pages/profile/ProfileIndex";
 import ListMyTicket from "../pages/profile/ListMyTicket";
+import DetailTicket from "../pages/profile/DetailTicket";
 
 export const PublicRoute: RouteObject[] = [
   {
@@ -82,5 +83,9 @@ export const PublicRoute: RouteObject[] = [
   {
     path: "/profile/my-ticket",
     element: <ListMyTicket />,
+  },
+  {
+    path: "/profile/my-ticket/:id",
+    element: <DetailTicket />,
   },
 ];
